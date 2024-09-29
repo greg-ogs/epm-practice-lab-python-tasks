9ab4c9cb4d4094b8b6d8ca74c1ecbd4498c5ebf1
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2024-09-29 02:54:24 -0600

    i try but...

--- conf.py
+++ conf.py
@@ -1,25 +1,14 @@
 import yaml
 from jinja2 import Template
 
-try:
-    with open('data.yml') as data_file:
-        config_data = yaml.load(data_file, Loader=yaml.FullLoader)
-except yaml.YAMLError as e:
-    print(f"Error parsing YAML: {e}")
-    exit(0)
-try:
-    with open('vhosts.j2') as template_file:
-        template_html = template_file.read()
-except IOError as e:
-    print(f"Error reading template file: {e}")
-    exit(0)
+with open('data.yml') as data_file:
+    config_data = yaml.load(data_file, Loader=yaml.FullLoader)
+
+with open('vhosts.j2') as template_file:
+    template_html = template_file.read()
 
 template = Template(template_html)
 vhosts_conf = template.render(config_data)
 
-try:
-    with open('vhosts.conf', 'w') as vhosts_file:
-        vhosts_file.write(vhosts_conf)
-except IOError as e:
-    print(f"Error writing output file: {e}")
-    exit(0)
+with open('vhosts.conf', 'w') as vhosts_file:
+    vhosts_file.write(vhosts_conf)
